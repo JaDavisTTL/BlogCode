0d7b1d64eebd461e5da36cde9dca2b8780ed0e37
JamesDa <james.davis@thetrainline.com>
2014-03-07 15:08:02 +0000

    Commented out the code in main prog

--- BlogDB/BlogDB/Program.cs
+++ BlogDB/BlogDB/Program.cs
@@ -14,9 +14,9 @@ namespace BlogDB
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ForBlogSite());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Blog()); 
         }
     }
 }
